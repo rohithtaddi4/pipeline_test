9290e99590c24d97a0cb5b29ee61d0047e1ff6ae
Rohith-Cog <142010338+Rohith-Cog@users.noreply.github.com>
2023-08-30 13:43:20 +0530

    env check

--- login_test.js
+++ login_test.js
@@ -2,17 +2,17 @@ Feature('login');
 
 Before(()=> {
 console.log('env var is')
-console.log(`$MY_URL`)
+console.log(process.env)
 })
 
 Scenario('test something',  ({ I }) => {
     console.log('env var is')
   I.amOnPage('https://codecept.io/playwright/#setup')
-  console.log(`$MY_URL`)
+  console.log(process.env)
 
 });
 
 After(()=> {
     console.log('env var is')
-    console.log($MY_URL)
+    //console.log($MY_URL)
 })
