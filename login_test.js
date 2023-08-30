1d13695915b115c9e0fb302a2cb91d523957eb30
Rohith-Cog <142010338+Rohith-Cog@users.noreply.github.com>
2023-08-30 13:28:23 +0530

    test pipeline

--- login_test.js
+++ login_test.js
@@ -1,5 +1,18 @@
 Feature('login');
 
+Before(()=> {
+console.log('env var is')
+console.log($MY_URL)
+})
+
 Scenario('test something',  ({ I }) => {
+    console.log('env var is')
   I.amOnPage('https://codecept.io/playwright/#setup')
+  console.log($MY_URL)
+
 });
+
+After(()=> {
+    console.log('env var is')
+    console.log($MY_URL)
+})
